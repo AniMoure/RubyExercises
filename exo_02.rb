14877008455de53bce2a2e6ee9e93a1645b97c5a
Anisoara <barbalatana@gmail.com>
2018-06-20 04:46:47 +0200

    Update exo_02.rb

--- exo_02.rb
+++ exo_02.rb
@@ -2,6 +2,6 @@ a = "Bonjour, monde !"
 puts a
 puts "Et avec une voix sexy, ça donne : " + a
 
-#puts insere une nouvelle ligne apres le string,
+#puts insere une nouvelle ligne apres la string,
 #tandis que print affiche les strings une apres
 #les autres
